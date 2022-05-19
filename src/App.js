e6fc682c6d64864ea21fbf4b546e8ad6441b7274
Setsofia-lab <setsofiaeli@gmail.com>
2022-05-19 20:40:32 +0000

    update api link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const sendData = async (e) => {
     e.preventDefault();
     await axios.post(
-      "http://localhost:3000",
+      "https://spacebarapi.herokuapp.com",
       { phone: phone, email: email },
       { headers: { "content-type": "application/json" } }
     );
